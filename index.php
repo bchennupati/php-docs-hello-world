3c2f197eca68550d454f51f45336093ccaef0163
bchennupati <127436029+bchennupati@users.noreply.github.com>
2023-04-04 13:51:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-
+$name="Balakrishna"
 echo "Hello World!";
+echo "Hi! my name is " . $name . "</br>";
+echo "Hi! my name is " . $name . "</br>";
+echo "Hi! my name is " . $name . "</br>";
+$name1="Navya"
+echo "Hello World!";
+echo "Hi! my name is " . $name1 . "</br>";
+echo "Hi! my name is " . $name1. "</br>";
+echo "Hi! my name is " . $name1. "</br>";
+?>
+  
+
